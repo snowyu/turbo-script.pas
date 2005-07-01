5ee52eb66e8235fb71ebe6e6e257e6b4d7bd0b65
Riceball <>
2005-07-01 01:40:57 +0000

    *** empty log message ***

--- Source/uProcessor.pas
+++ Source/uProcessor.pas
@@ -13,27 +13,37 @@ type
   PAddress = ^TAddress;
   TAddress = record
   end;
+  {: Code Field Address }
+  TCodeField = packed record
+    {: 调用方式 }
+    CallStyle: Byte;
+    NextCFA: Pointer;
+  end;
 
-  TMemoryArray = array of Integer;
-  TStack = TMemoryArray; 
+  TMemoryArray = array of byte;
+  TStack = TMemoryArray;
 
   TInstruction = (
     inPrepare,
     {## Arithmatic instuctions }
-    inAdd, //Add
-    inSub, //subtract
-    inInc, //add 1
-    inDec, //subtract 1
-    inMUL, //multiply 
-    inDIV, //divide
+    //the integer operation
+    inAddInt, //Add
+    inSubInt, //subtract
+    inIncInt, //add 1
+    inIncNInt, //add N
+    inDecInt, //subtract 1
+    inDecNInt, //subtract N
+    inMULInt, //multiply 
+    inDIVInt, //divide
 
     {## Logical instuctions }
-    inEQU,
-    inNEQ, // not equ
-    inLES, //less than
-    inLEQ, //less than and equ
-    inGET, //greater than
-    inGEQ, //greater than and equ
+    //the integer operation
+    inEQUInt,
+    inNEQInt, // not equ
+    inLESInt, //less than
+    inLEQInt, //less than and equ
+    inGERInt, //greater than
+    inGEQInt, //greater than and equ
     inNOT, //Negate(NOT)
     inAND,
     inOR,
@@ -52,13 +62,17 @@ type
     inNoop,
 
     {## Stack Operation Instuction }
-    inPush,
-    inPop,
-    inDrop,
-    inDUP,
-    inSWAP,
-    inOVER,
-    inROT
+    inRSPush //Push Integer
+    , inRSPop //Pop Integer
+
+    {## Parameter Stack Operation Instuction }
+    , inPush  //Push Integer
+    //, inPop //the same as the Drop
+    , inDrop
+    , inDUP
+    , inSWAP
+    , inOVER
+    , inROT
   ); 
   { Summary : The Virtual Machine Code }
   { Description:
@@ -70,7 +84,7 @@ type
   end;
   
 type
-  TCustomProcessor = class (TObject)
+  TCustomProcessor = class(TObject)
   private
     FMemorySize: Integer;
     FStackSize: Integer;
@@ -83,8 +97,12 @@ type
     FSP: Integer;
     FStatus: TProcessorStates;
     Stack: TStack;
+    procedure iRSPop;
+    procedure iRSPush;
   public
-    procedure Execute; virtual; abstract;
+    procedure Execute(const aAddress: Integer = 0); virtual;
+    procedure ExecuteInstruction(const aInstruction: TInstruction); virtual;
+    procedure Init; virtual;
     procedure LoadFromFile(const aFileName: String);
     procedure LoadFromStream(const aStream: TStream); virtual; abstract;
     procedure SaveToFile(const aFile: String);
@@ -98,10 +116,26 @@ type
     property Status: TProcessorStates read FStatus;
   end;
   
-  TStackProcessor = class (TCustomProcessor)
+  TStackProcessor = class(TCustomProcessor)
+  private
+    FParameterStack: TStack;
+    FParameterStackSize: Integer;
+    PSP: Integer;
+    procedure SetParameterStackSize(const Value: Integer);
+  public
+    procedure ExecuteInstruction(const aInstruction: TInstruction); override;
+    procedure Init; override;
+    property ParameterStackSize: Integer read FParameterStackSize write
+            SetParameterStackSize;
   end;
   
-  TForthProcessor = class (TStackProcessor)
+  TForthProcessor = class(TStackProcessor)
+  protected
+    FWorkingRegister: Integer;
+    FXRegister: Integer;
+  public
+    procedure iExecute;
+    procedure iNext;
   end;
   
 
@@ -110,6 +144,36 @@ implementation
 {
 ******************************* TCustomProcessor *******************************
 }
+procedure TCustomProcessor.Execute(const aAddress: Integer = 0);
+begin
+  Init;
+  //
+  while FPC < Length(FMemory) do
+  begin
+    FIR := TInstruction(@FMemory[FPC]);
+    ExecuteInstruction(FIR);
+    Inc(FPC, SizeOf(TInstruction));
+  end;
+end;
+
+procedure TCustomProcessor.ExecuteInstruction(const aInstruction: TInstruction);
+begin
+end;
+
+procedure TCustomProcessor.Init;
+begin
+  PC := 0;
+  SP := StackSize;
+end;
+
+procedure TCustomProcessor.iRSPop;
+begin
+end;
+
+procedure TCustomProcessor.iRSPush;
+begin
+end;
+
 procedure TCustomProcessor.LoadFromFile(const aFileName: String);
 var
   aFileStream: TFileStream;
@@ -150,6 +214,91 @@ begin
     FStackSize := Value;
     SetLength(FStack, FStackSize);
   end;
+end;
+
+{
+******************************* TStackProcessor ********************************
+}
+procedure TStackProcessor.ExecuteInstruction(const aInstruction: TInstruction);
+var
+  LTemp: Integer;
+begin
+  inherited ExecuteInstruction(aInstruction);
+  case aInstruction of
+    inAddInt:
+    begin
+      LTemp := Integer(@FParameterStack[PSP]);
+      Inc(PSP, SizeOf(Integer));
+      FParameterStack[PSP] := FParameterStack[PSP] + LTemp;
+    end;
+    inSubInt:
+    begin
+      LTemp := Integer(@FParameterStack[PSP]);
+      Inc(PSP, SizeOf(Integer));
+      FParameterStack[PSP] := FParameterStack[PSP] - LTemp;
+    end;
+    inIncInt:
+    begin
+      Inc(Integer(@FParameterStack[PSP]));
+    end;
+    inDecInt:
+    begin
+      Dec(Integer(@FParameterStack[PSP]));
+    end;
+    inIncNInt:
+    begin
+      LTemp := Integer(@FParameterStack[PSP]);
+      Inc(PSP, SizeOf(Integer));
+      Inc(Integer(@FParameterStack[PSP]), LTemp);
+    end;
+    inDecNInt:
+    begin
+      LTemp := Integer(@FParameterStack[PSP]);
+      Inc(PSP, SizeOf(Integer));
+      Dec(Integer(@FParameterStack[PSP]), LTemp);
+    end;
+    inMULInt:
+    begin
+      LTemp := Integer(@FParameterStack[PSP]);
+      Inc(PSP, SizeOf(Integer));
+      FParameterStack[PSP] := FParameterStack[PSP] * LTemp;
+    end;
+    inDIVInt:
+    begin
+      LTemp := Integer(@FParameterStack[PSP]);
+      Inc(PSP, SizeOf(Integer));
+      FParameterStack[PSP] := FParameterStack[PSP] div LTemp;
+    end;
+  end;
+end;
+
+procedure TStackProcessor.Init;
+begin
+  inherited Init;
+  PSP := FParameterStackSize;
+end;
+
+procedure TStackProcessor.SetParameterStackSize(const Value: Integer);
+begin
+  if FParameterStackSize <> Value then
+  begin
+    FParameterStackSize := Value;
+    SetLength(FParameterStack, FParameterStackSize);
+  end;
+end;
+
+{
+******************************* TForthProcessor ********************************
+}
+procedure TForthProcessor.iExecute;
+begin
+end;
+
+procedure TForthProcessor.iNext;
+begin
+  FWorkingRegister := Integer(@FMemory[FPC]);
+  //JMP (FWorkingRegister)
+  FPC := FWorkingRegister;
 end;
 
 
